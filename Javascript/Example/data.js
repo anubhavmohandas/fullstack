c09f69591fba0a6d9c95a5ddd92800769aecb3ed
anubhavmohandas <anubhav.manav147@gmail.com>
2022-06-28 21:53:50 +0530

    js

---
+++ Javascript/Example/data.js
@@ -0,0 +1,10 @@
+alert(HACKED);
+
+// const para = document.querySelector('p');
+
+// para.addEventListener('click', updateName);
+
+// function updateName() {
+//   const name = prompt('Enter a new name');
+//   para.textContent = `Player 1: ${name}`;
+// }
--- Javascript/java.js
+++ Javascript/java.js
@@ -1,1 +1,42 @@
-alert('Hey Buddy');
+// alert('Hey Buddy');
+
+// variables = var
+
+var a = 1;
+var b = 2;
+var result = a + b
+console.log(result)
+3
+
+var a = "Anubhav"
+console.log(a)
+
+document.getElementById('me').innerHTML='Namaste';
+
+// prompt('what is your age');
+// var age = prompt
+// console.log(age);
+
+// document.getElementById('me').innerHTML= age;
+
+// Numbers
+var a = 1
+var b = 2
+a+b
+3
+var a = 1
+var b = 2
+a-b
+-1
+var a = 1
+var b = 2
+b-a
+1
+var a = 1
+var b = 2
+b/a
+2
+var a = 1
+var b = 2
+b*a
+2
