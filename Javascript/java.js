af54f5de1c6e112e0687a6e38e2c697e91a3ddc7
anubhavmohandas <anubhav.manav147@gmail.com>
2022-06-30 20:53:47 +0530

    js

--- Javascript/java.js
+++ Javascript/java.js
@@ -22,6 +22,7 @@ document.getElementById('me').innerHTML='Namaste';
 // Numbers
 var a = 1
 var b = 2
+
 a+b
 3
 var a = 1
@@ -40,3 +41,73 @@ var a = 1
 var b = 2
 b*a
 2
+
+var n1 = 10
+var n2 = 20
+
+n1 = n1+1
+11
+n2 = n2+1
+21
+n1
+11
+n1 = n1-1
+10
+n1
+10
+n1++
+
+n1
+11
+n1++
+11
+n1--
+12
+n1
+11
+n1--
+11
+n1
+10
+
+// FUNCTION
+/* Create A Function
+    Call A Function */
+
+// function fun(){
+//     alert('Hey! I am a Function');
+// }
+
+// fun();
+
+// function wish(){
+//     prompt("What is your Name?");
+// }
+// wish()
+
+    // var name = prompt("What is your Name?");
+    // var result="Hello " + name;
+    // var result="Hello" + "" + name; //String Concatenation
+    // console.log(result)
+
+
+// Arguements in a Function
+
+function add(n1, n2){
+    let result= n1 + n2;
+    console.log(result)
+}
+
+add(10, 10);
+20
+
+// Loops
+/*  1. While Loop
+    2. For Loop */
+
+/* While Loop*/
+let num=0
+while (num < 100){
+    num+1;
+    console.log(num)
+}
