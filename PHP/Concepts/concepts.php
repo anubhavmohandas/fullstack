159d3c7c0b75679bc6a305096bdc6100c3d99ab6
anubhavmohandas <anubhav.manav147@gmail.com>
2022-07-18 21:03:50 +0530

    php

---
+++ PHP/Concepts/concepts.php
@@ -0,0 +1,39 @@
+<html>
+    <body>
+        <?php
+            // PHP CODE GOES HERE
+
+            $str = "Hello, world!";
+            $num = 5;
+            $float = 0.5;
+
+            ------------------------------
+
+            $mobile = array("oneplus" , "realme" , "oppo");
+            var_dump ($mobile [2]);
+            //echo (var_dump ($mobile [2]));
+
+            //CONSONANTS
+
+            define ("mobile", "Oneplus , Redmi and Vivo");
+            echo mobile;
+
+            //Conditional Statements 
+            /*
+                -> If
+                -> If Else
+                -> Else If (elif)
+                -> Switch
+            */
+
+            $var = 1;
+            if  ($var > 5) {
+                echo $var;
+            }
+
+            else {
+                echo "Condition not met";
+            }
+        ?>
+    </body>
+</html>
---
+++ PHP/index.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <h1>HI</h1>
+        <?php
+            echo "Helloooooo"
+            echo "Namastee"
+        ?>
+    </body>
+</html>
