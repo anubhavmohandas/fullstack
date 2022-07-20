8dda3b4d935e4a5e7fb58744f4b88b6a5e5b7d29
anubhavmohandas <anubhav.manav147@gmail.com>
2022-07-20 21:04:30 +0530

    php

--- PHP/Concepts/concepts.php
+++ PHP/Concepts/concepts.php
@@ -34,6 +34,70 @@
             else {
                 echo "Condition not met";
             }
+
+            // IF ELSE ELSE IF
+            $var = 9;
+            if ($var %2 == 0 && $var %3 == 0) {
+                echo "Divisible by both.";
+                }
+
+                elseif ($var %2 == 0) {
+                    echo "Divisible by 2";
+                }
+
+                elseif ($var %3 == 0) {
+                    echo "Divisible by 3";
+                }
+
+                else {
+                    echo "Divisible by NONE (Neither 2 nor 3)";
+                }
+
+            // Switch Case
+            $pokemon = 9;
+                switch ($pokemon) {
+                    case 1: 
+                        echo "You are on Level 1";
+                        break;
+
+                    case 2:
+                        echo "You are on Level 2";
+                        break;
+                    
+                    case 3:
+                        echo "You are on Level 3";
+                        break;
+
+                    default:
+                        echo "Invalid Pokemon.";
+                }
+                
+            //LOOPS
+            /*
+                -> WHILE LOOP
+                -> DO LOOP
+                -> FOR LOOP
+            */
+            
+            // While loop
+             $var = -5;
+             while ($var <= 10) {
+                echo "The number is: $var <br>";
+                $var++;
+            }
+
+            // Do Loop
+            $var = 0;
+            do {
+                echo "The number is: $var <br>";
+                $var++;
+            } while ($var <= 15)
+
+            // FOR LOOP
+            for ($var = 0; $var <= 10; $var++) {
+                echo "The number is: $var <br>";
+            }            
         ?>
+
     </body>
 </html>
